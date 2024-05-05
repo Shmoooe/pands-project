193d26f124f01f52504c2d438330f14652742435
Joanna Kelly <joannabkelly@gmail.com>
2024-05-05 23:43:11 +0100

    sepal_length_hist created

--- analysis.py
+++ analysis.py
@@ -7,9 +7,27 @@
 import pandas as pd    # imports pandas library and assigns the nickname 'pd'
 
 iris = pd.read_csv("iris.csv") # Reads the data from a csv file and stores it in a pandas data frame
-
 description = iris.describe() # This creates summary statistics for each variable
-print(description)
 
+
+'''
 with open("summary_statistics.txt", "w") as file:     # Creates a txt file 
     file.write(description.to_string())         # writes to the new file and converts the summary stats to a string
+'''
+
+import matplotlib.pyplot as plt # software to create my histograms and scatter plots
+
+setosa = iris[iris["species"]== "setosa"]  #seperates the species for later analysis
+versicolor = iris[iris["species"]== "versicolor"]
+virginica = iris[iris["species"]== "virginica"]
+
+iris["sepal_length"].hist()  # isolates sepal length column
+#setosa["sepal_length"].hist()
+#versicolor["sepal_length"].hist()
+#virginica["sepal_length"].hist()
+plt.xlabel("Length in mm") # Adds a label to the x axis
+plt.ylabel("Frequency")
+plt.title("Histogram of Sepal Lengths") # Adds a title to the histogram
+plt.show()  # displays the histogram when the program is run
+#plt.savefig("sepal_length_hist.png")          # saves the histogram to a PNG file
+
