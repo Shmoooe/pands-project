05936a29a16df3475f0822239b3d06b36cea205e
Joanna Kelly <joannabkelly@gmail.com>
2024-05-09 22:53:32 +0100

    scatter plots created

--- analysis.py
+++ analysis.py
@@ -21,74 +21,96 @@ virginica = iris[iris["species"]== "virginica"]
 
 fig, axes= plt.subplots(nrows=2, ncols=2, figsize=(10, 6)) # Creates a four histogram plot, number of rows and columns specified and the size of the figure in inches
 iris["sepal_length"].hist(ax=axes[0,0], color="darkgreen", alpha=0.7)  # isolates sepal length column and assigns it to row 1, column 1, alpha refers to the opacity
-axes[0,0].set_xlabel("Length in mm") # labels the x axis, along the bottom
+axes[0,0].set_xlabel("Length in cm") # labels the x axis, along the bottom
 axes[0,0].set_ylabel("Frequency") # labels the y axis, up the side
 axes[0,0].set_title("Sepal Length") # gives a title to this individual histogram
 
 iris["sepal_width"].hist(ax=axes[0,1], color="darkgreen", alpha=0.7)
-axes[0,1].set_xlabel("Width in mm")
+axes[0,1].set_xlabel("Width in cm")
 axes[0,1].set_ylabel("Frequency")
 axes[0,1].set_title("Sepal Width")
 
 iris["petal_length"].hist(ax=axes[1,0], color="#5E47CB") # hexadecimal colour of random iris flower
-axes[1,0].set_xlabel("Length in mm")
+axes[1,0].set_xlabel("Length in cm")
 axes[1,0].set_ylabel("Frequency")
 axes[1,0].set_title("Petal Length")
 
 iris["petal_width"].hist(ax=axes[1,1], color="#5E47CB", alpha= 0.8)
-axes[1,1].set_xlabel("Width in mm")
+axes[1,1].set_xlabel("Width in cm")
 axes[1,1].set_ylabel("Frequency")
 axes[1,1].set_title("Petal Width")
 plt.tight_layout()  # prevents overlapping of subplots
 plt.savefig("variable_overview_hist.png")          # saves the histogram to a PNG file
-plt.show()  # displays the histogram when the program is run
+#plt.show()  # displays the histogram when the program is run
 
 
 
 fig1, axes1= plt.subplots(nrows=1, ncols=3, figsize=(8, 5)) # Creates a four histogram plot, number of rows and columns specified and the size of the figure in inches
 setosa["sepal_length"].hist(ax=axes1[0], color= "darkgreen", alpha=0.8) #axes1[0]= the first histogram of the row in column one
 axes1[0].set_title("Setosa Sepal Length")
+axes1[0].set_xlabel("in cm")
 
 versicolor["sepal_length"].hist(ax=axes1[1], color= "darkgreen", alpha=0.75)
 axes1[1].set_title("Versicolor Sepal Length")
+axes1[1].set_xlabel("in cm")
 
 virginica["sepal_length"].hist(ax=axes1[2], color= "darkgreen", alpha=0.7)
 axes1[2].set_title("Virginica Sepal Length")
-#plt.savefig("sepal_length_comparison.png")
-#plt.show()
+axes1[2].set_xlabel("in cm")
+
+plt.savefig("sepal_length_comparison.png")
+
+
+
 
 fig2, axes2= plt.subplots(nrows=1, ncols=3, figsize=(8,5))
 setosa["sepal_width"].hist(ax=axes2[0], color= "darkgreen", alpha=0.8)
 axes2[0].set_title("Setosa Sepal Width")
+axes2[0].set_xlabel("in cm")
 
 versicolor["sepal_width"].hist(ax=axes2[1], color= "darkgreen", alpha=0.75)
 axes2[1].set_title("Versicolor Sepal Width")
+axes2[1].set_xlabel("in cm")
 
 virginica["sepal_width"].hist(ax=axes2[2], color= "darkgreen", alpha=0.7)
 axes2[2].set_title("Virginica Sepal Width")
-#plt.savefig("sepal_width_comparison.png")
-#plt.show()
+axes2[2].set_xlabel("in cm")
+plt.savefig("sepal_width_comparison.png")
+
 
 fig3, axes3= plt.subplots(nrows=1, ncols=3, figsize=(8,5))
 setosa["petal_length"].hist(ax=axes3[0], color="#5E47CB", alpha=0.8)
 axes3[0].set_title("Setosa Petal Length")
+axes3[0].set_xlabel("in cm")
 
 versicolor["petal_length"].hist(ax=axes3[1], color="#5E47CB", alpha=0.75)
 axes3[1].set_title("Versicolor Petal Length")
+axes3[1].set_xlabel("in cm")
 
 virginica["petal_length"].hist(ax=axes3[2], color="#5E47CB", alpha=0.7)
 axes3[2].set_title("Virginica Petal Length")
-#plt.savefig("petal_length_comparison.png")
-#plt.show()
+axes3[2].set_xlabel("in cm")
+plt.savefig("petal_length_comparison.png")
+
 
 fig4, axes4= plt.subplots(nrows=1, ncols=3, figsize=(8,5))
 setosa["petal_width"].hist(ax=axes4[0], color="#5E47CB", alpha=0.8)
 axes4[0].set_title("Setosa Petal Width")
+axes4[0].set_xlabel("in cm")
 
 versicolor["petal_width"].hist(ax=axes4[1], color="#5E47CB", alpha=0.75)
 axes4[1].set_title("Versicolor Petal Width")
+axes4[1].set_xlabel("in cm")
 
 virginica["petal_width"].hist(ax=axes4[2], color="#5E47CB", alpha=0.7)
 axes4[2].set_title("Virginica Petal Width")
-#plt.savefig("petal_width_comparison.png")
-#plt.show()
+axes4[2].set_xlabel("in cm")
+plt.savefig("petal_width_comparison.png")
+
+iris.plot.scatter(x="sepal_length", y="sepal_width", color= "darkgreen", alpha=0.5)
+plt.title("Sepal Length and Width in cm")
+plt.savefig("sepal_scatter.png")
+
+iris.plot.scatter(x="petal_length", y="petal_width", color="#5E47CB", alpha=0.5)
+plt.title("Petal Length and Width in cm")
+plt.savefig("petal_scatter.png")
